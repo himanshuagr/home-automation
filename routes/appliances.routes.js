2607fe884bc64133109c5e344fad1ba0451946df
himanshu Agrawal <agrawalhimanshu99@gmail.com>
2020-06-10 20:02:05 +0530

    abc

--- routes/appliances.routes.js
+++ routes/appliances.routes.js
@@ -19,52 +19,20 @@ router.get('/status',(req,res)=>{
       });
      
       
-     appliances.findById({_id:id},(error,response)=>{
-       if(error)
-         return res.send(error);
+     appliances.findById({_id:id}).then((response)=>{
+      
+      console.log(response);
+      console.log(req.query);
+      res.send({
+        messgae:"oooooo"
+      })
 
-         
-       if(req.query.type=='temperature')
-          res.json({
-            temperature:response.temperature
-          });
-          else if(req.query.type=='humidity')
-          res.json({
-            humidity:response.humidity
-          });
-          else if(req.query.type=='gas_level')
-          res.json({
-            gas_level:response.gas_level
-          });
-          else if(req.query.type=='cooler')
-          res.json({
-            cooler:response.cooler
-          });
-          else if(req.query.type=='fan')
-          res.json({
-            fan:response.fan
-          });
-          else if(req.query.type=='light1')
-          res.json({
-            light1:response.light1
-          });
-          else if(req.query.type=='light2')
-          res.json({
-            light2:response.light2
-          });
-          else if(req.query.type=='light3')
-          res.json({
-            light3:response.light3
-          });
-          else if(req.query.type=='motion')
-          res.json({
-            motion:response.motion
-          });
-          else
-            res.status(500).json({
-              message: "invalid query"
-            })
+     }).catch((error)=>{
+       res.status(404).send(error);
      })
+      
+         
+  
 })
 
 router.post('/switch',(req,res)=>{
