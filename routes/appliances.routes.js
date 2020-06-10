2ec150e98acef725ef30c6d32e288f14e9dfaf22
himanshu Agrawal <agrawalhimanshu99@gmail.com>
2020-06-10 20:17:55 +0530

    A

--- routes/appliances.routes.js
+++ routes/appliances.routes.js
@@ -19,21 +19,59 @@ router.get('/status',(req,res)=>{
       });
      
       
-     appliances.findById({_id:id}).then((response)=>{
-      
-      console.log(response);
-      console.log(req.query);
-      res.send({
-        messgae:"oooooo"
-      })
+     appliances.findById({_id:id},(error,response)=>{
+       if(error)
+         return res.send(error);
 
-     }).catch((error)=>{
-       res.status(404).send(error);
-     })
-      
-         
-  
-})
+        if(response)
+        { 
+       if(req.query.type=='temperature')
+          res.json({
+            temperature:response.temperature
+          });
+          else if(req.query.type=='humidity')
+          res.json({
+            humidity:response.humidity
+          });
+          else if(req.query.type=='gas_level')
+          res.json({
+            gas_level:response.gas_level
+          });
+          else if(req.query.type=='cooler')
+          res.json({
+            cooler:response.cooler
+          });
+          else if(req.query.type=='fan')
+          res.json({
+            fan:response.fan
+          });
+          else if(req.query.type=='light1')
+          res.json({
+            light1:response.light1
+          });
+          else if(req.query.type=='light2')
+          res.json({
+            light2:response.light2
+          });
+          else if(req.query.type=='light3')
+          res.json({
+            light3:response.light3
+          });
+          else if(req.query.type=='motion')
+          res.json({
+            motion:response.motion
+          });
+          else
+            res.status(500).json({
+              message: "invalid query"
+            });
+          }
+          else
+           res.send({
+             message:"no response recieved"
+           })
+     });
+});
 
 router.post('/switch',(req,res)=>{
   if(!req.body)
