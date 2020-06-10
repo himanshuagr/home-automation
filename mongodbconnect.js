e4bf1f26f288b7bda37a015b44620bf3765506f0
himanshu Agrawal <agrawalhimanshu99@gmail.com>
2020-06-10 17:47:21 +0530

    AA

--- mongodbconnect.js
+++ mongodbconnect.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 const appliance=require('./models/appliance.model');
 const mongourl="mongodb+srv://him9875:hyx110@home-automation-isox0.gcp.mongodb.net/appliances?retryWrites=true&w=majority";
-mongoose.connect(process.env.MONGODB_URI || mongourl,{
+mongoose.connect(process.env.MONGODB_URI,{
     useCreateIndex:true,
     useNewUrlParser:true
 }).then(()=>console.log("database connected successfully"))
